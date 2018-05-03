63605df02f5ea5d8ce81c2ad439567293c860828
Salvatore Santagati <salvatore.santagati@gmail.com>
2018-05-03 12:07:22 +0200

    refactor: move functions outside class

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 // Modal window
-/* Model Variable */
-/* Get the modal */
+// Model Variable
+// Get the modal
 let modal = document.getElementById('myModal');
-/* Get the button that opens the modal */
+// Get the button that opens the modal
 let btn = document.getElementById('myBtn');
 let playButton = document.querySelector('button');
 let modalWindow = document.getElementById('myModal');
 let modalMessage = document.querySelector('.modal-message');
 modalWindow.style.display = 'none';
 
-/* Level Game*/
+// Level Game
 var levelGame = 1;
 var levelGameCounter = document.querySelector('.myLevel');
 
 var displayResult = document.querySelector('.container');
 
-/* Time Variables*/
+// Time Variables
 var startGame = 0;
 var initTime = 0;
 var t = 0;
@@ -23,13 +23,50 @@ var timerBoardId;
 var timerBoard = document.querySelector('.myTime');
 
 
-/* Manage time of game */
+// Manage time of game
 function initTimerBoard() {
   timerBoard.innerText = t + 's';
   t++;
 }
 
-/* Return a random block start enemy  */
+// Check Collision from Player and Enemies
+// Axis-Aligned Bounding Box
+// Original Source
+// https://developer.mozilla.org/kab/docs/Games/Techniques/2D_collision_detections
+function checkCollision() {
+  var result = 0;
+  allEnemies.forEach(function(collision) {
+    if (player.x < collision.x + collision.width &&
+      player.x + player.width > collision.x &&
+      player.y < collision.y + collision.height &&
+      player.height + player.y > collision.y) {
+      result = 1;
+    }
+  });
+  return result;
+}
+
+function nextLevel() {
+  // Add enemy if player pass level
+  if (allEnemies.length < levelGame) {
+    allEnemies.push(new Enemy('images/enemy-bug.png', -150, randomOrdinate()));
+  }
+}
+
+//reset variable and game
+function finishGame() {
+  clearInterval(timerBoardId);
+  modalWindow.style.display = 'block';
+  displayResult.style.display = 'none';
+
+  // show result
+  modalMessage.innerText = 'FINISH\n Level ' + levelGame + ' \n' + 'Time : ' + t + 'seconds';
+
+  player.y = 420;
+
+}
+
+// Return a random block start enemy
 function randomOrdinate() {
   let stoneBlock = [60, 143, 226];
   let index = Math.floor(Math.random() * 3);
@@ -38,6 +75,7 @@ function randomOrdinate() {
 
 // Enemies our player must avoid
 var Enemy = function(imgEnemy, abscissa, ordinate) {
+
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
   this.x = abscissa;
@@ -61,42 +99,21 @@ Enemy.prototype.update = function(dt) {
   // which will ensure the game runs at the same speed for
   // all computers.
   if (this.x < 500) {
-    if (this.speed > (1000 * dt)) {
+    if (this.speed > (1000)) {
       this.speed = Math.floor(Math.random() * 1000 * dt) + 1;
     } else this.x += this.speed;
   } else {
-    this.speed = Math.floor(Math.random() * this.speed + 1000 * dt);
+    this.speed += Math.floor(this.speed * dt * Math.random(1000));
     this.x = -150 - Math.floor(5000 * Math.random());
     this.y = randomOrdinate();
   }
 
-  // Check Collision from Player and Enemies
-  // Axis-Aligned Bounding Box
-  // Original Source
-  // https://developer.mozilla.org/kab/docs/Games/Techniques/2D_collision_detections
-
-  allEnemies.forEach(function(collision) {
-    if (player.x < collision.x + collision.width &&
-      player.x + player.width > collision.x &&
-      player.y < collision.y + collision.height &&
-      player.height + player.y > collision.y) {
-
-      // reset variable and game
-      clearInterval(timerBoardId);
-      modalWindow.style.display = 'block';
-      displayResult.style.display = 'none';
-
-      // show result
-      modalMessage.innerText = 'FINISH\n Level ' + levelGame + ' \n' + 'Time : ' + t + 'seconds';
-
-      player.y = 420;
-
-    }
-  });
-
-  /* Add enemy if player pass level */
-  if (allEnemies.length < levelGame) {
-    allEnemies.push(new Enemy('images/enemy-bug.png', -150, randomOrdinate()));
+  // if true end game
+  if (checkCollision() === 1) {
+    finishGame();
+  } else {
+    // Add enemy if player pass level
+    nextLevel();
   }
 };
 
@@ -108,7 +125,7 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(imgPlayer) {
+var Player = function(imgPlayer, speed) {
   if (startGame === 0) {
     startGame = 1;
     timerBoardId = setInterval(function() {
@@ -123,16 +140,13 @@ var Player = function(imgPlayer) {
   this.width = 70;
   this.height = 60;
 
-  this.sprite = imgPlayer;
+  this.speed = speed;
 
+  this.sprite = imgPlayer;
 };
 
-// Update the playuer position, required method for game
-// Parameter: dt, a time delta between ticks
-Player.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
+
+Player.prototype.update = function() {
 
   if (this.y === -30) {
     levelGame++;
@@ -152,16 +166,16 @@ Player.prototype.handleInput = function(direction) {
 
   switch (direction) {
     case 'up':
-      if (this.y > -60) this.y -= 30;
+      if (this.y > -60) this.y -= 30 * this.speed;
       break;
     case 'down':
-      if (this.y < 420) this.y += 30;
+      if (this.y < 420) this.y += 30 * this.speed;
       break;
     case 'left':
-      if (this.x > 0) this.x -= 25;
+      if (this.x > 0) this.x -= 25 * this.speed;
       break;
     case 'right':
-      if (this.x < 400) this.x += 25;
+      if (this.x < 400) this.x += 25 * this.speed;
       break;
 
     default:
@@ -177,7 +191,7 @@ Player.prototype.handleInput = function(direction) {
 var allEnemies = [];
 
 allEnemies.push(new Enemy('images/enemy-bug.png', -150, randomOrdinate()));
-var player = new Player('images/char-cat-girl.png');
+var player = new Player('images/char-cat-girl.png', 1 );
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -192,7 +206,7 @@ document.addEventListener('keyup', function(e) {
   player.handleInput(allowedKeys[e.keyCode]);
 });
 
-/* restart button  */
+// restart button
 playButton.addEventListener('click', function() {
   document.location.reload(true);
 });
